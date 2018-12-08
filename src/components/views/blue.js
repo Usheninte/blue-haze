e398e9b0e4e3bcae0f4722c09a30cbca5cfa010f
nullthefirst <jrandangana@gmail.com>
2018-12-08 19:04:21 +0100

    edited blue

--- src/components/views/blue.js
+++ src/components/views/blue.js
@@ -1,11 +1,4 @@
 import React , { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-
-import Un from './1un';
-
-const foam = {
-  margin: "1em"
-}
 
 export default class Blue extends Component {
   render() {
@@ -17,17 +10,6 @@ export default class Blue extends Component {
               <img src={'https://res.cloudinary.com/poetrique/image/upload/v1544283930/nullthefirst/Nullthefirst_blue-HAZE.png'} alt="blue HAZE" title="blue HAZE" />
             </div>
           </div>
-          <Router>
-            <div style={foam} className="row">
-              <div className="col">
-                <Link to="/un">
-                  <button type="button" class="btn btn-danger">BEGIN</button>
-                </Link>
-
-                <Route path="/un" component={Un} />
-              </div>
-            </div>
-          </Router>
         </div>
       </div>
     );
