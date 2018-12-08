021106944594f0de18a783415a7ff5842189395b
nullthefirst <jrandangana@gmail.com>
2018-12-08 19:03:00 +0100

    edited 1un

--- src/components/views/1un.js
+++ src/components/views/1un.js
@@ -1,11 +1,4 @@
 import React , { Component } from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
-
-import Blue from './blue';
-
-const foam = {
-  margin: "1em"
-}
 
 export default class Un extends Component {
   render() {
@@ -17,17 +10,6 @@ export default class Un extends Component {
               <img src={''} alt="blue HAZE" title="blue HAZE" />
             </div>
           </div>
-          <Router>
-            <div style={foam} className="row">
-              <div className="col">
-                <NavLink to="/">
-                  <button type="button" class="btn btn-danger">COVER</button>
-                </NavLink>
-
-                <Route exact path="/" component={Blue} />
-              </div>
-            </div>
-          </Router>
         </div>
       </div>
     );
