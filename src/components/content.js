d48348ad86206d76ca64263386dba3f11b453191
nullthefirst <jrandangana@gmail.com>
2018-12-08 19:26:10 +0100

    added to content menu

--- src/components/content.js
+++ src/components/content.js
@@ -4,9 +4,11 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Un from './views/1un';
 import Deux from './views/2deux';
 import Trois from './views/3trois';
+import Quatre from './views/4quatre';
+import Cinq from './views/5cinq';
 
 const foam = {
-  margin: "1em"
+  margin: "1.5em"
 }
 
 export default class Content extends Component {
@@ -15,6 +17,7 @@ export default class Content extends Component {
       <Router>
         <div className="container-fluid">
           <div style={foam} className="row">
+            <div className="col"></div>
             <div className="col">
               <Link to="/un">
                 <button type="button" class="btn btn-danger">1 - UN</button>
@@ -30,11 +33,29 @@ export default class Content extends Component {
                 <button type="button" class="btn btn-danger">3 - TROIS</button>
               </Link>
             </div>
+            <div className="col"></div>
           </div>
-        
+
+          <div style={foam} className="row">
+            <div className="col"></div>
+            <div className="col">
+              <Link to="/quatre">
+                <button type="button" class="btn btn-danger">4 - QUATRE</button>
+              </Link>
+            </div>
+            <div className="col">
+              <Link to="/cinq">
+                <button type="button" class="btn btn-danger">5 - CINQ</button>
+              </Link>
+            </div>
+            <div className="col"></div>
+          </div>
+
           <Route path="/un" component={Un} />
           <Route path="/deux" component={Deux} />
           <Route path="/trois" component={Trois} />
+          <Route path="/quatre" component={Quatre} />
+          <Route path="/cinq" component={Cinq} />
         </div>
       </Router>
     );
---
+++ src/components/views/5cinq.js
@@ -0,0 +1,17 @@
+import React , { Component } from 'react';
+
+export default class Cinq extends Component {
+  render() {
+    return(
+      <div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col">
+              <img src={''} alt="blue HAZE" title="blue HAZE" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
