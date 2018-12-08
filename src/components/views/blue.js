a64c53db5d84d850788a4e41d2e023eef00d72de
nullthefirst <jrandangana@gmail.com>
2018-12-08 16:13:29 +0100

    editing cover

--- src/components/views/blue.js
+++ src/components/views/blue.js
@@ -6,9 +6,6 @@ const title = {
   paddingBottom: "1em"
 }
 
-const status = {
-}
-
 const one = {
   textAlign: "left",
   margin: "2em"
@@ -19,18 +16,36 @@ const two = {
   margin: "3em"
 }
 
+const three = {
+  textAlign: "left",
+  margin: "4.5em"
+}
+
+const four = {
+  textAlign: "right",
+  margin: "0.5em",
+  padding: "2em"
+}
+
+const five = {
+  textAlign: "left",
+  margin: "2.5em"
+}
+
 export default class Blue extends Component {
   render() {
     return(
       <div>
         <div id="cover">
-          <h2 style={title}>blue haze</h2>
+          <h2 style={title}>b-l-u-e _ h-a-z-e</h2>
           <hr />
-          <div style={status}>
+          <div id="status">
             <p style={one}>a tale</p>
             <p style={two}>of love</p>
-            <p>and the</p>
-            <p>lack of it</p>
+            <p style={three}>and the</p>
+            <p style={four}>lack of it</p>
+            <hr />
+            <p style={five}>... that's it</p>
           </div>
         </div>
       </div>
