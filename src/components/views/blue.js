21a63773c2e9609166a9271ce47f66cbedcb3a7c
nullthefirst <jrandangana@gmail.com>
2018-12-08 15:29:10 +0100

    building cover

--- src/components/views/blue.js
+++ src/components/views/blue.js
@@ -1,14 +1,12 @@
 import React , { Component } from 'react';
 
-const hey = {
-  margin: "5em"
-}
-
 export default class Blue extends Component {
   render() {
     return(
       <div>
-        <h1 style={hey}>Hello World</h1>
+        <div id="cover">
+          <h1>blue haze</h1>
+        </div>
       </div>
     );
   }
