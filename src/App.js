5fc6a0621a98434aeb5be634cdd54a559f32f4d6
nullthefirst <jrandangana@gmail.com>
2018-12-08 19:16:02 +0100

    built content menu foundations

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './components/App.css';
 
 import Blue from './components/views/blue';
+import Content from './components/content';
 import Footer from './components/footer';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div>
         <Blue />
+        <Content />
         <Footer />
       </div>
     );
--- src/components/content.js
+++ src/components/content.js
@@ -1,11 +1,15 @@
 import React , { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import Un from './views/1un';
+import Deux from './views/2deux';
+import Trois from './views/3trois';
+
 const foam = {
   margin: "1em"
 }
 
-export default class Blue extends Component {
+export default class Content extends Component {
   render() {
     return(
       <Router>
@@ -13,12 +17,24 @@ export default class Blue extends Component {
           <div style={foam} className="row">
             <div className="col">
               <Link to="/un">
-                <button type="button" class="btn btn-danger">BEGIN</button>
+                <button type="button" class="btn btn-danger">1 - UN</button>
+              </Link>
+            </div>
+            <div className="col">
+              <Link to="/deux">
+                <button type="button" class="btn btn-danger">2 - DEUX</button>
+              </Link>
+            </div>
+            <div className="col">
+              <Link to="/trois">
+                <button type="button" class="btn btn-danger">3 - TROIS</button>
               </Link>
             </div>
           </div>
-
+        
           <Route path="/un" component={Un} />
+          <Route path="/deux" component={Deux} />
+          <Route path="/trois" component={Trois} />
         </div>
       </Router>
     );
