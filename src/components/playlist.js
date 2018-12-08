4f1b0dc3f0a2445e3d69d6756277be773dee16f4
nullthefirst <jrandangana@gmail.com>
2018-12-08 20:01:08 +0100

    added soudcloud iframe

--- src/components/playlist.js
+++ src/components/playlist.js
@@ -5,11 +5,12 @@ export default class Play extends Component {
   render() {
     return(
       <div>
-        <Iframe url="http://www.youtube.com/embed/xDMP3i36naA"
+        <Iframe url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/659141667&color=%23ed6165&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
         width="90%"
         height="auto"
         display="initial"
         position="relative"
+        allow="autoplay"
         allowFullScreen/>
       </div>
     );
