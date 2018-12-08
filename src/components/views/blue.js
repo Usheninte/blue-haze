28a2475c3f6b7820c741a4442d0fbc11c1e362f8
nullthefirst <jrandangana@gmail.com>
2018-12-08 18:22:33 +0100

    added bootstrap 4

--- src/components/views/blue.js
+++ src/components/views/blue.js
@@ -1,11 +1,25 @@
 import React , { Component } from 'react';
 
+const foam = {
+  margin: "1em"
+}
+
 export default class Blue extends Component {
   render() {
     return(
       <div>
-        <img src={'https://res.cloudinary.com/poetrique/image/upload/v1544283930/nullthefirst/Nullthefirst_blue-HAZE.png'} />
-        <input type="button" value="BEGIN" />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col">
+              <img src={'https://res.cloudinary.com/poetrique/image/upload/v1544283930/nullthefirst/Nullthefirst_blue-HAZE.png'} />
+            </div>
+          </div>
+          <div style={foam} className="row">
+            <div className="col">
+              <button type="button" class="btn btn-danger">BEGIN</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
