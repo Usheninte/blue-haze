8fa5fbabb45b787b3109d92a84d1bdf4f272c5b2
nullthefirst <jrandangana@gmail.com>
2018-12-08 15:58:10 +0100

    adding status

--- src/components/views/blue.js
+++ src/components/views/blue.js
@@ -1,11 +1,29 @@
 import React , { Component } from 'react';
 
+const title = {
+  textAlign: "right",
+  fontStyle: "italic",
+  paddingBottom: "1em"
+}
+
+const status = {
+  textAlign: "left",
+  margin: "0.8em"
+}
+
 export default class Blue extends Component {
   render() {
     return(
       <div>
         <div id="cover">
-          <h1>blue haze</h1>
+          <h2 style={title}>blue haze</h2>
+          <hr />
+          <div style={status}>
+            <p>a tale</p>
+            <p>of love</p>
+            <p>and the</p>
+            <p>lack of it</p>
+          </div>
         </div>
       </div>
     );
