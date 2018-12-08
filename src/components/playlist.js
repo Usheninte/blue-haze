2c2af375baff6901e8f3c1911e9f151463012b7c
nullthefirst <jrandangana@gmail.com>
2018-12-08 20:45:46 +0100

    edited iframe styling

--- src/components/playlist.js
+++ src/components/playlist.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Iframe from 'react-iframe';
 
+const wave = {
+  margin: "2.5em"
+}
+
 export default class Play extends Component {
   render() {
     return(
-      <div>
+      <div style={wave}>
         <Iframe url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/659141667&color=%23ed6165&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
         width="90%"
         height="auto"
