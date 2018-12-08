2f62253b5f4510178630a1356753735c589ee6bd
nullthefirst <jrandangana@gmail.com>
2018-12-08 18:26:25 +0100

    added fontawesome

---
+++ src/components/footer.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+const footerStyle = {
+  textAlign: "center",
+  marginTop: "4em"
+}
+
+const blueBold = {
+  color: "#61dafb",
+  fontStyle: "bold"
+}
+
+export default class Footer extends Component {
+  render() {
+    return(
+      <div style={footerStyle}>
+        <a
+          href="https://github.com/Usheninte"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h5>nullthefirst <i style={blueBold} class="fas fa-code"></i> 2018</h5> 
+        </a>
+      </div>
+    );
+  }
+}
