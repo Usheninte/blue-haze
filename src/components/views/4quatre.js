5edf24e6a4a108c24d858f7111bb1566ada689c1
nullthefirst <jrandangana@gmail.com>
2018-12-08 19:07:12 +0100

    added 4quatre

---
+++ src/components/views/4quatre.js
@@ -0,0 +1,17 @@
+import React , { Component } from 'react';
+
+export default class Quatre extends Component {
+  render() {
+    return(
+      <div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col">
+              <img src={''} alt="blue HAZE" title="blue HAZE" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
